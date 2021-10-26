ee357d0722f97e5e50d5a9ea203ca6b2ae45f8eb
Estefania Urro <77628759+EstefaniaUrro@users.noreply.github.com>
2021-10-26 12:01:25 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import GestorBuscador from './Componentes/Buscador/GestorBuscador';
 import Detalle from './Componentes/paginas/Detalle';
 import Footer from './Componentes/Footer';
@@ -9,12 +9,17 @@ import NotFound from './Componentes/NotFound';
 function App() {
   return (
     <div>
-      <Switch>
+//       <Switch>
+//         <Route exact path='/' component={GestorBuscador} />
+//         <Route path='/detalle/:id' component={Detalle} />
+//         <Route component={NotFound} />
+//       </Switch>
+//       <Footer />
+    <Router history={browserHistory}>
         <Route exact path='/' component={GestorBuscador} />
         <Route path='/detalle/:id' component={Detalle} />
-        <Route component={NotFound} />
-      </Switch>
-      <Footer />
+        <Route path='/notFound' component={NotFound} />
+  </Router>
     </div>
   )
 }
